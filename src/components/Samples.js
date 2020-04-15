281e7c34e88a6fcc1a8a11f552524eb1df51abda
Sonica1031 <Sonica1031@hotmail.com>
2020-04-15 00:22:33 -0500

    upgrades

--- src/components/Samples.js
+++ src/components/Samples.js
@@ -5,10 +5,10 @@ function Samples(){
     <div>
         <h1>Stephanie's Samples</h1>
         <div class="linking-links">
-        <a class="link-tag" html="https://custom-hooks.netlify.com/">Custom Hooks</a>
-        <a class="link-tag" html="https://yourtodos.netlify.com/">Todo List</a>
-        <a class="link-tag" html="https://my-giga-pet.netlify.com/">Team Project</a>
-        
+        <a class="link-tag" href="https://custom-hooks.netlify.com/">Custom Hooks</a>
+        <a class="link-tag" href="https://yourtodos.netlify.com/">Todo List</a>
+        <a class="link-tag" href="https://my-giga-pet.netlify.com/">Team Project</a>
+
         </div>
     </div>
     );
