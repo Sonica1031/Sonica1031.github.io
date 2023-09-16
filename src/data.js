50e2155f40fbb41d68ab053db191e98b8db9a5e0
Sonica1031 <sonica1031@hotmail.com>
2023-09-16 12:11:47 -0500

    Revert "Updating old website"
    This reverts commit a54fe9d9e8b8cc856c6c59746639b93835a8eaa3.

---
+++ src/data.js
@@ -0,0 +1,106 @@
+export const workHistory= [{
+    id:1,
+    job_title: "Cashier/File Maintenance",
+    place_of_business: "Stop & Shop",
+    date: "November 2010 - March 2013",
+    address: "4531 Main Street, Bridgeport, CT 06606",
+    phone_number: "(203) 371 - 6972"
+},
+{
+    id:2,
+    job_title: "Cashier/File Maintenance",
+    place_of_business: "Target",
+    date: "July 2013 – June 2014",
+    address: "8315 N Rockwell Ave, OKC, OK 73132",
+    phone_number: "(405) 506 - 6086"
+},
+{
+    id:3,
+    job_title: "Overflow",
+    place_of_business: "Target",
+    date: "July 2013 – June 2014",
+    address: "Norman",
+    phone_number: "unavailable"
+},
+{
+    id:4,
+    job_title: "Cashier",
+    place_of_business: "Homeland",
+    date: "August 2014 – April 2015",
+    address: "1724 W Lindsey St, Norman, OK 73069",
+    phone_number: "(405) 321 - 1313"
+},
+{
+    id:5,
+    job_title: "Cashier",
+    place_of_business: "Wal-Mart Neighborhood Market",
+    date: "April 2015 – September 2016",
+    address: "11101 N Rockwell Ave, OKC, OK 73162",
+    phone_number: "(405) 773 - 3555"
+},
+{
+    id:6,
+    job_title: "Pharmacy Technician",
+    place_of_business: "Walgreens",
+    date: "September 2016 – January 2018",
+    address: "5901 NW 122nd St, OKC, OK 73132",
+    phone_number: "(405) 722 - 1356"
+},
+{
+    id:7,
+    job_title: "Pharmacy Technician",
+    place_of_business: "Mercy Hospital",
+    date: "January 2018 – August 2019",
+    address: "4300 W Memorial Rd, OKC, OK 73120",
+    phone_number: "(405) 755 - 1515"
+},
+{
+    id:8,
+    job_title: "Enterprise Help Desk Technician",
+    place_of_business: "Dell EMC",
+    date: "September 2019 - December 2020",
+    address: "3501 SW 15th St, Oklahoma City, OK 73108",
+    phone_number: "(800) 456 - 3355"
+},
+{
+    id:9,
+    job_title: "Network Operation Center Intern",
+    place_of_business: "NetFabric",
+    date: "December 2020 - May 2021",
+    address: "1141 W. Sheridan Avenue, Suite J, Oklahoma Ckity, OK 73106",
+    phone_number: "405-570-4872"
+},
+{
+    id:10,
+    job_title: "Applications Update Tier II",
+    place_of_business: "BankOnIT",
+    date: "May 2021 - Present",
+    address: "8601 Commerce Park Dr, Oklahoma City, OK 73132",
+    phone_number: "405-605-3900"
+}
+];
+
+export const navItems = [{
+        id: "1",
+        title: "Home",
+        value: ""
+},
+{
+ 
+    id: "2",
+    title: "About Me",
+    value: "about-me"
+},
+{
+    id: "3",
+    title: "Work experience",
+    value: "work-experience"
+},
+{
+    id: "4",
+    title: "Samples",
+    value: "Samples"
+}
+];
+
+export default workHistory;
